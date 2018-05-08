1837499a3a300842e409a28749b2919c302ebce0
krismadden <madden_kris@yahoo.com>
2018-05-08 19:19:42 +0200

    Update testKeyboad.py

--- testKeyboad.py
+++ testKeyboad.py
@@ -1,10 +1,20 @@
 import curses
 
-while True:
-	try:
-		if keyboard.is_pressed('q'):
-			print("You pressed a key!!!")
-			break
-		else:
-			pass
-	except: break
+def main(win):
+    win.nodelay(True)
+    key=""
+    win.clear()                
+    win.addstr("Detected key:")
+    while 1:          
+        try:                 
+           key = win.getkey()         
+           win.clear()                
+           win.addstr("Detected key:")
+           win.addstr(str(key)) 
+           if key == os.linesep:
+              break           
+        except Exception as e:
+           # No input   
+           pass         
+
+curses.wrapper(main)
