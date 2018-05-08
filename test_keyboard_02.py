661702b0c6bd3c12d0c56e889d1869181f57e2ee
krismadden <madden_kris@yahoo.com>
2018-05-08 19:50:47 +0200

    Update test_keyboard_02.py

--- test_keyboard_02.py
+++ test_keyboard_02.py
@@ -4,6 +4,8 @@ import pyttsx
 voiceEngine = pyttsx.init()
 voiceEngine.setProperty('rate', 150)
 
+message = ""
+
 def speak(str):
 	if len(sys.argv) > 1:
 		str = sys.argv[1]
@@ -12,21 +14,26 @@ def speak(str):
 
 def getchar():
    #Returns a single character from standard input
-   import tty, termios, sys
-   fd = sys.stdin.fileno()
-   old_settings = termios.tcgetattr(fd)
-   try:
-      tty.setraw(sys.stdin.fileno())
-      ch = sys.stdin.read(1)
-   finally:
-      termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-   return ch
+	import tty, termios, sys
+	fd = sys.stdin.fileno()
+	old_settings = termios.tcgetattr(fd)
+	try:
+		tty.setraw(sys.stdin.fileno())
+		ch = sys.stdin.read(1)
+	finally:
+		termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+	return ch
    
 while 1:
-    ch = getchar()
-    if ch.strip() == '':
-        print('bye!')
-        break
-    else:
-        print 'You pressed', ch
-        speak(ch)
+	ch = getchar()
+	if ch.strip() == '':
+		print('bye!')
+		speak("bye")
+		break
+	if ch.strip() == '=':
+		speak(message)
+
+	else:
+		print 'You pressed', ch
+		speak(ch)
+		message = message + ch
