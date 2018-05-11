74f31fd00225f4acc7d37977155f259edc308494
krismadden <madden_kris@yahoo.com>
2018-05-11 20:15:08 +0200

    Update m590_setup.py

--- m590_setup.py
+++ m590_setup.py
@@ -4,7 +4,7 @@ import serial
 import time
 
 class m590:
-	ser = serial.Serial("/dev/ttyAMA0", 128000, timeout=5)
+	ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=5)
 # 	ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=5)
 	SMS = {}
 	PHONE = {}
