650d8c23c088a50127bc8590b1c65351dc33248a
krismadden <madden_kris@yahoo.com>
2018-05-11 13:59:28 +0200

    Update 01_texting.py

--- 01_texting.py
+++ 01_texting.py
@@ -45,7 +45,7 @@ def setUpPin():
 		response = m590.ser.readlines(None)
 		print (response)
 
-		if response[0] == "OK\r\n" or response[1]) == "OK\r\n" or response[2] == "OK\r\n":
+		if response[0] == "OK\r\n" or response[1] == "OK\r\n" or response[2] == "OK\r\n":
 			print ("pin okay. let's go.")
 	# 		speak("pin okay. let's go.")
 			break
