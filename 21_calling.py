4c469a27d1bf61625a3e76c943d00684ee08a087
krismadden <madden_kris@yahoo.com>
2018-05-13 16:47:19 +0200

    Update 21_calling.py

--- 21_calling.py
+++ 21_calling.py
@@ -101,7 +101,6 @@ def main():
 		if keyboard.is_pressed('space'):
 			runProgram = False
 		response = m590.ser.read(None)
-		print (response)
 		
 		if len(response) > 3:
 			while response[1] == "RING\r\n" or response[3] == "RING\r\n":
