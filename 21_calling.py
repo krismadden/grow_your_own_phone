a6d362cb2c450b270b52587f2d5689b20925c215
krismadden <madden_kris@yahoo.com>
2018-05-13 15:54:25 +0200

    Update 21_calling.py

--- 21_calling.py
+++ 21_calling.py
@@ -101,7 +101,7 @@ def main():
 		m590.ser.write("at\r")
 		response = m590.ser.readlines(None)
 		print (response)
-		if len(response) > 2:
+		if len(response) > 3:
 			while response[1] == "RING\r\n" or response[3] == "RING\r\n":
 				if keyboard.is_pressed('1'):
 					m590.ser.write("ata\r")
