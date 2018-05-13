2b4aa9b55da7734cffbfd2a7d9c67dde31788353
krismadden <madden_kris@yahoo.com>
2018-05-13 19:10:56 +0200

    Update 21_calling.py

--- 21_calling.py
+++ 21_calling.py
@@ -98,7 +98,7 @@ def main():
 	while runProgram:
 		if keyboard.is_pressed('space'):
 			runProgram = False
-		response = m590ser.read(None)
+		response = m590.ser.read(None)
 		print (response)
 		
 		if len(response) > 3:
