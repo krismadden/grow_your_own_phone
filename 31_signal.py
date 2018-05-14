6877b424ea1967045c8c7008d5631db6522275c0
krismadden <madden_kris@yahoo.com>
2018-05-14 21:42:13 +0200

    Update 31_signal.py

--- 31_signal.py
+++ 31_signal.py
@@ -4,11 +4,25 @@ import time
 import RPi.GPIO as GPIO
 import serial
 
-	
+from neopixel import *
+
+LED_COUNT   = 14      # Number of LED pixels.
+LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
+LED_INVERT  = False   # True to invert the signal (when using NPN transistor level shift)
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
+# Intialize the library (must be called once before other functions).
+strip.begin()
+
+colorWipe(strip, Color(0, 255, 0))  # Blue wipe
 
 ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
 def main():
+	
+	
 	
 	checkSignalStrength = True
 	
