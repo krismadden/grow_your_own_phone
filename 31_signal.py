ea10af109c3c1c2b7011e323b57ac764ca72799b
krismadden <madden_kris@yahoo.com>
2018-05-14 23:04:13 +0200

    Update 31_signal.py

--- 31_signal.py
+++ 31_signal.py
@@ -4,18 +4,24 @@ import time
 import RPi.GPIO as GPIO
 import serial
 
-import neopixel
+# import neopixel
 
-LED_COUNT   = 9      # Number of LED pixels.
-LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
-LED_INVERT  = False   # True to invert the signal (when using NPN transistor level shift)
+# LED_COUNT   = 9      # Number of LED pixels.
+# LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
+# LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+# LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
+# LED_INVERT  = False   # True to invert the signal (when using NPN transistor level shift)
 
-strip = neopixel.Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
-# Intialize the library (must be called once before other functions).
-strip.begin()
+# strip = neopixel.Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT)
+# # Intialize the library (must be called once before other functions).
+# strip.begin()
 
+#setup LEDs#
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.output(18,GPIO.LOW)
+#end setup for LEDs#
 
 
 
@@ -24,7 +30,16 @@ ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
 
 def main():
 	random = 0
-	
+	GPIO.output(18,GPIO.LOW)
+	time.sleep(2)
+	GPIO.output(18,GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(18,GPIO.LOW)
+	time.sleep(2)
+	GPIO.output(18,GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(18,GPIO.LOW)
+	time.sleep(2)
 	checkSignalStrength = True
 	
 	print("checking signal strength")
@@ -34,10 +49,10 @@ def main():
 		ser.write("at+CSQ\r")
 		response = ser.readlines(None)
 		print(response)
-		for i in range(0,8):
-			strip.setPixelColor(i, neopixel.Color(random, 0, 255))
-			strip.show()
-		time.sleep(20.0)
+# 		for i in range(0,8):
+# 			strip.setPixelColor(i, neopixel.Color(random, 0, 255))
+# 			strip.show()
+# 		time.sleep(20.0)
 
 
 if __name__ == "__main__":
