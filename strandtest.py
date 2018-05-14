8e377ebc831c186f8d6b74282e10f2578acb13f1
krismadden <madden_kris@yahoo.com>
2018-05-14 23:21:39 +0200

    Update strandtest.py

--- strandtest.py
+++ strandtest.py
@@ -11,7 +11,7 @@ import argparse
 import serial
 
 # LED strip configuration:
-LED_COUNT      = 9      # Number of LED pixels.
+LED_COUNT      = 6      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
@@ -96,7 +96,7 @@ def main():
 	
 	while checkSignalStrength:
 		ser.write("at+CSQ\r")
-		rainbowCycle(strip)
+		colorWipe(strip, Color(0, 255, 0))  # Green wipe
 		response = ser.readlines(None)
 		print(response)
         
