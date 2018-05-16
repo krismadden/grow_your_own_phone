c0274c0478b6fdd77d02e6a32fb24fba78e60bdd
krismadden <madden_kris@yahoo.com>
2018-05-16 17:57:06 +0200

    Update 23_calling.py

--- 23_calling.py
+++ 23_calling.py
@@ -43,11 +43,8 @@ def vibrate():
 	print("vibrate")
 	p.start(7.5)
 	p.ChangeDutyCycle(7.5)
-	time.sleep(1)
-	p.ChangeDutyCycle(7.5)
-	time.sleep(1)
+	p.ChangeDutyCycle(12.5)
 	p.ChangeDutyCycle(2.5)
-	time.sleep(1)
 	p.stop
 
 def setUpPin():
