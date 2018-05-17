6830625dc88240968f96369cf677f3baa8d1bcaf
krismadden <madden_kris@yahoo.com>
2018-05-17 15:24:40 +0200

    Update 24_calling_greenhouse.py

--- 24_calling_greenhouse.py
+++ 24_calling_greenhouse.py
@@ -69,23 +69,23 @@ def setUpPin():
 		response = m590.ser.readlines(None)
 		time.sleep(0.5)
 		print (response)
-
-		if response[0] == "OK\r\n" or response[1] == "OK\r\n" or response[2] == "OK\r\n":
-			print ("pin okay. let's go.")
-			break
-		elif response[1] == "+CPIN: READY\r\n" or response[2] != "+CPIN: READY\r\n":
-			print ("pin okay. let's go.")
-			break
-		elif response[2] == "+CPIN: SIM PIN\r\n":
-			m590.ser.write("at+cpin=\"1234\"\r")
-			time.sleep(0.5)
-			continue
-		elif response[1] == "ERROR/r/n" or response[2] == "ERROR/r/n":
-			print (response[1] + "\n")
-			print ("Error. Restart the Module")
-		else:
-			print (response[1] + "\n")
-			print ("check your SIM card is inserted and the light on the GSM module is flashing./nIf all looks good, get Kris.")
+		if len(response) > 0:
+			if response[0] == "OK\r\n" or response[1] == "OK\r\n" or response[2] == "OK\r\n":
+				print ("pin okay. let's go.")
+				break
+			elif response[1] == "+CPIN: READY\r\n" or response[2] != "+CPIN: READY\r\n":
+				print ("pin okay. let's go.")
+				break
+			elif response[2] == "+CPIN: SIM PIN\r\n":
+				m590.ser.write("at+cpin=\"1234\"\r")
+				time.sleep(0.5)
+				continue
+			elif response[1] == "ERROR/r/n" or response[2] == "ERROR/r/n":
+				print (response[1] + "\n")
+				print ("Error. Restart the Module")
+			else:
+				print (response[1] + "\n")
+				print ("check your SIM card is inserted and the light on the GSM module is flashing./nIf all looks good, get Kris.")
 
 def restart():
 	command = "/usr/bin/sudo /sbin/shutdown -r now"
