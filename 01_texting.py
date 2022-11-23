36672ba9d4019b23184bc85333b0788e28dd57cc
krismadden <madden_kris@yahoo.com>
2022-11-23 13:40:05 +0100

    Update 01_texting.py

--- 01_texting.py
+++ 01_texting.py
@@ -90,7 +90,8 @@ def setUpPin():
 		time.sleep(0.3)
 		m590.ser.write("at+cpin?\r")
 		response = m590.ser.readlines(None)
-		print ("response is:    " + response)
+		print ("response is:    ")
+		print(response)
 
 		if response[0] == "OK\r\n" or response[1] == "OK\r\n" or response[2] == "OK\r\n":
 			print ("pin okay. let's go.")
