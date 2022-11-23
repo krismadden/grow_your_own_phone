d4523ba2ca7e302d36f683d4aff6c213cdf2ac57
krismadden <madden_kris@yahoo.com>
2022-11-23 14:43:38 +0100

    Update 01_texting.py

--- 01_texting.py
+++ 01_texting.py
@@ -90,11 +90,13 @@ def setUpPin():
 		time.sleep(0.3)
 		m590.ser.write("at+cpin?\r")
 		response = m590.ser.readlines(None)
-		print ("response is:    ")
-		print(response)
+		
 		
 		if response == "":
-			response[0] = "error with gsm module"
+			response.append('error with gsm module')
+		
+		print ("response is:    ")
+		print(response)
 		
 		
 		if response[0] == "OK\r\n" or response[1] == "OK\r\n" or response[2] == "OK\r\n":
